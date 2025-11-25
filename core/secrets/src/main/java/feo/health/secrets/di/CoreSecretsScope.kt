54001a8dfc44d8d43724f4b25a0c873812beabed
Aleksej Timko <aleksejtmk@gmail.com>
2025-11-25 14:57:55 +0300

    Add core secrets

--- core/secrets/src/main/java/feo/health/secrets/di/CoreSecretsScope.kt
+++ core/secrets/src/main/java/feo/health/secrets/di/CoreSecretsScope.kt
@@ -1,7 +1,7 @@
-package feo.health.datastore.datastore.di
+package feo.health.secrets.di
 
 import javax.inject.Scope
 
 @Scope
 @Retention(AnnotationRetention.RUNTIME)
-annotation class DatastoreScope
+annotation class CoreSecretsScope

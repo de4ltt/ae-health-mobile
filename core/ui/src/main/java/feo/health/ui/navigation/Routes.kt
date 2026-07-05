728902d8eb10f43d4dd9e3c495820e50f39fff05
Aleksej Timko <aleksejtmk@gmail.com>
2026-07-05 12:30:36 +0300

    refactor: centralize Kotlin serialization navigation routes in core:ui

--- core/ui/src/main/java/feo/health/ui/navigation/Routes.kt
+++ core/ui/src/main/java/feo/health/ui/navigation/Routes.kt
@@ -1,5 +1,7 @@
 package feo.health.ui.navigation
 
+import kotlinx.serialization.Serializable
+
 object Routes {
     val catalog
         get() = "catalog"
@@ -12,3 +14,30 @@ object Routes {
     val ai
         get() = "ai"
 }
+
+@Serializable
+object CatalogRoute
+
+@Serializable
+data class CatalogDetailsRoute(val link: String, val type: String)
+
+@Serializable
+data class CatalogSpecialistsRoute(val link: String)
+
+@Serializable
+object AiRoute
+
+@Serializable
+object AuthRoute
+
+@Serializable
+object AuthLogOutRoute
+
+@Serializable
+object UserRoute
+
+@Serializable
+object UserFavouritesRoute
+
+@Serializable
+object UserHistoryRoute

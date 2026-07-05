aa835b1fbee2f38c4b58febfe70bf61db1a41048
Aleksej Timko <aleksejtmk@gmail.com>
2026-07-05 15:31:37 +0300

    refactor: delete unused class HStrings, annotation DefaultDispatcher, and unused Colors.kt file inside core modules

--- core/ui/src/main/java/feo/health/ui/dispatcher/DefaultDispatcher.kt
+++
@@ -1,8 +0,0 @@
-package feo.health.ui.dispatcher
-
-
-/**
- * Dagger qualifier annotation used to bind and identify the CPU-bound default [CoroutineDispatcher].
- */
-@Retention(AnnotationRetention.RUNTIME)
-annotation class DefaultDispatcher
--- core/ui/src/main/java/feo/health/ui/resource/HStrings.kt
+++
@@ -1,6 +0,0 @@
-package feo.health.ui.resource
-
-/**
- * Static string resources configuration helper container.
- */
-object HStrings
